663852d00508f26ae070ebb636eacccb85c7536c
Samuel Tambunan <samuel.tambunan@au.experian.com>
2014-08-11 17:11:26 +1000

    Added JSon States

---
+++ DirectoryUpdatesSLN/Data/JsonStates.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+///This class is used to represent a Json state for the dataset update value
+namespace com.qas.sambo.directoryupdate.Data
+{
+    class JsonStates
+    {
+        public string Dataset { get; set; }
+        public string ServerLocation { get; set; }
+        public List<string> DownloadLocation { get; set; }
+        public string LastModifiedDate { get; set; }
+    }
+}
