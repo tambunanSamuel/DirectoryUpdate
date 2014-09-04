fbb5dcf6a1cc3317de8edcfd036b5d5aa7b81e62
Samuel Tambunan <tambunansamuel@gmail.com>
2014-09-04 23:13:53 +1000

    Final CopyLearning

--- com.qas.sambo.CSharpLearning/CopyLearning.cs
+++ com.qas.sambo.CSharpLearning/CopyLearning.cs
@@ -30,10 +30,10 @@ namespace com.qas.sambo.CSharpLearning
                        for (int i = 0; i < size; i++)
                        {
                            fs.WriteByte(fileSize[i]);
-                           //if (i % 4000 == 0)
+                           if (i % 10000 == 0)
                            {
                                fcc.progress = (ulong)i;
-                               Console.WriteLine("Copied {0} from {1} , percentage {2:00}%", i, size, (float)i / size*100);
+                               //Console.WriteLine("Copied {0} from {1} , percentage {2:00}%", i, size, (float)i / size*100);
                                prog.Report(fcc);
                            }
                        }
@@ -47,9 +47,9 @@ namespace com.qas.sambo.CSharpLearning
         public static void p_ProgressChanged(object sender, FileCopiedClass e)
         {
             int pos = Console.CursorTop;
-            Console.WriteLine("Pos is {0}", pos);
+            //Console.WriteLine("Pos is {0}", pos);
             Console.WriteLine("Progress Copied: {0:0.00}%" , (float)e.progress/e.actualSize*100);
-            Console.WriteLine("Pos is now {0}", Console.CursorTop);
+            //Console.WriteLine("Pos is now {0}", Console.CursorTop);
             Console.SetCursorPosition(0, pos);
         }
 
