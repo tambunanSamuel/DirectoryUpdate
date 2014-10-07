66df872c8c64ba83b283589e7f1e67ffa2445a98
Samuel Tambunan <samuel.tambunan@au.experian.com>
2014-10-07 15:18:41 +1100

    before changing copyDirectory

---
+++ com.qas.sambo.CSharpLearning/CopyDirectory.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace com.qas.sambo.CSharpLearning
+{
+    class CopyDirectory
+    {
+        public static void CopyDirectoryMethod(string oldDirectory, string newDirectory)
+        {
+            var dirList = from dir in Directory.EnumerateDirectories(oldDirectory)
+                          let dirInfo = new DirectoryInfo(dir)
+                          orderby dirInfo.CreationTime ascending
+                          select dir;
+
+            foreach (var f in dirList)
+            {
+                Console.WriteLine("Directory {0}",f);
+            }
+        }
+
+        public static void Main()
+        {
+            CopyDirectory.CopyDirectoryMethod(@"C:\MyFiles","");
+        }
+    }
+}
